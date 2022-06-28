51b34de5d1e1d764f041cd98676604afefd2d14b
Rkono7 <rostkonovod@gmail.com>
2022-06-28 17:38:27 +0300

    FieldModels
    Models of field playiyng added!

---
+++ CourseWork/Field/FieldModel.cs
@@ -0,0 +1,9 @@
+﻿using CourseWork.Field.Abstract;
+
+namespace CourseWork.Field
+{
+    public class FieldModel : AbstractField
+    {
+        public FieldModel(int width, int height) : base(width, height) { }
+    }
+}
